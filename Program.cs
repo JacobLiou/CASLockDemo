3a6c95e7b61e2cc17942fa6c92711e1cae022d1c
liumenghui <liumenghui@genomics.cn>
2022-02-18 16:09:43 +0800

    Task 所有锁的使用 C# --java-C++

--- Program.cs
+++ Program.cs
@@ -4,11 +4,25 @@ namespace CASLockDemo;
 
 public class Program
 {
-
-
     private static long _currentTime;
     private static long _current;
     private static Semaphore _semaphore = new Semaphore(0, 10);
+
+    public static long FixedWindow()
+    {
+       
+        var now = DateTimeOffset.Now.ToUnixTimeSeconds();
+        var ct = Interlocked.Read(ref _currentTime);
+        if (now > ct)
+        {
+            if (Interlocked.CompareExchange(ref _currentTime, now, ct) == ct)
+            {
+                Interlocked.Exchange(ref _current, 0);
+            }
+        }
+
+        return Interlocked.Increment(ref _current);
+    }
 
     static void Main(string[] args)
     {
@@ -20,22 +34,38 @@ public class Program
             {
                 for (int j = 0; j < 10000; j++)
                 {
-                    Cas.FixedWindow();
+                    FixedWindow();
                 }
 
                 _semaphore.Release(1);
             });
         }
-
+        //等待全部信号量释放
         for (int i = 0; i < 10; i++)
         {
             _semaphore.WaitOne();
         }
 
+        // var tasks = new Task[10];
+        // for (int i = 0; i < tasks.Length; ++i)
+        // {
+        //     var task = Task.Run(() =>
+        //     {
+        //         for (int j = 0; j < 10000; j++)
+        //         {
+        //             FixedWindow();
+        //         }
+        //     });
+        //     tasks[i] = task;
+        // }
+
+
+        // Task.WhenAll(tasks).Wait();
+
         Console.WriteLine(_current);
         Console.WriteLine("sleep 2s");
         Thread.Sleep(2000);
-        Console.WriteLine(Cas.FixedWindow());
+        Console.WriteLine(FixedWindow());
     }
 
 
